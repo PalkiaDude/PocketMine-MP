28d1cb66cf41690e04d86a2ea80aac378ffdd83c
PalkiaDude <teammegamanz@gmail.com>
2016-04-29 19:22:02 +0800

    Moo

--- src/pocketmine/entity/Cow.php
+++ src/pocketmine/entity/Cow.php
@@ -21,7 +21,55 @@
 
 namespace pocketmine\entity;
 
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\item\Item as ItemItem;
+use pocketmine\network\Network;
+use pocketmine\network\protocol\AddEntityPacket;
+use pocketmine\Player;
 
 class Cow extends Animal{
+  const NETWORK_ID = 11;
 
+    public $width = 1.6;
+    public $height = 1.12;
+    
+    public function getName(){
+      return "Cow";
+    }
+
+	public function spawnTo(Player $player){
+		$pk = new AddEntityPacket();
+		$pk->eid = $this->getId();
+		$pk->type = Cow::NETWORK_ID;
+		$pk->x = $this->x;
+		$pk->y = $this->y;
+		$pk->z = $this->z;
+		$pk->speedX = $this->motionX;s
+		$pk->speedY = $this->motionY;
+		$pk->speedZ = $this->motionZ;
+		$pk->yaw = $this->yaw;
+		$pk->pitch = $this->pitch;
+		$pk->metadata = $this->dataProperties;
+		$player->dataPacket($pk);
+		parent::spawnTo($player);
+	}
+	
+    public function targetOption(Player $player, $distance){
+        return $player->spawned && $player->isAlive() && !$player->closed && $player->getInventory()->getItemInHand()->getId() == Item::WHEAT && $distance <= 49;
+    }
+
+    public function getDrops(){
+        $drops = [];
+        if($this->lastDamageCause instanceof EntityDamageByEntityEvent){
+            switch(mt_rand(0, 1)){
+                case 0 :
+                    $drops[] = Item::get(Item::RAW_BEEF, 0, 1);
+                    break;
+                case 1 :
+                    $drops[] = Item::get(Item::LEATHER, 0, 1);
+                    break;
+            }
+        }
+        return $drops;
+    }
 }
